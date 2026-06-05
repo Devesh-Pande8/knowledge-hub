efac50f1322db151157aa986fff8caf5ee987b71
Devesh Pande <deveshanandpandeytl@gmail.com>
2026-06-05 12:35:43 +0530

    updated the content length

--- src/validations/knowledge.schema.ts
+++ src/validations/knowledge.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const knowledgeSchema = z.object({
   title: z.string().min(3),
-  content: z.string().min(10),
+  content: z.string().min(4),
   tags: z.array(z.string()).optional(),
   fileUrl: z.string().optional(),
 });
